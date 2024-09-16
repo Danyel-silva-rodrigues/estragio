23164549663fc6995962da71aba3f68a9c8f56b1
Danyel-silva-rodrigues <155700370+Danyel-silva-rodrigues@users.noreply.github.com>
2024-09-16 12:09:16 -0300

    navbar resposiva
    soluçao do poblemar com a nav bar resposiva adeptaçao para mobile ;)

--- src/js/home.js
+++ src/js/home.js
@@ -1,18 +1,34 @@
-let userLogado = JSON.parse(localStorage.getItem('userLogado'))
+document.addEventListener('DOMContentLoaded', () => {
+    let userLogado = JSON.parse(localStorage.getItem('userLogado'));
 
-let logado = document.querySelector('#logado')
+    let logado = document.querySelector('#logado');
+    if (userLogado) {
+        logado.innerHTML = `Olá ${userLogado.Nome}`;
+    } else {
+        logado.innerHTML = 'Usuário não encontrado';
+    }
 
-logado.innerHTML= `ola ${userLogado.Nome}`
+    if (localStorage.getItem('token') == null) {
+        alert('Você precisa estar logado para acessar essa página');
+        window.location.href = 'login.html';
+    }
 
-if (localStorage.getItem('token') == null){
-    alert('voce precisa estar logado para acessar essa pagina')
-    window.location.href ='login.html'
-}
+    let hamburger = document.querySelector('.hamburger');
+    let navBar = document.querySelector('.nav-bar');
+
+   
+        hamburger.addEventListener('click', () => {
+            navBar.classList.toggle('active');
+            
+        });
+   
+
+    window.sair = function() {
+        localStorage.removeItem('token');
+        localStorage.removeItem('userLogado');
+        window.location.href = 'login.html';
+    };
+});
 
 
-function sair() {
-    localStorage.removeItem('token')
-    localStorage.removeItem('userLogado')
-    window.location.href ='login.html'
-    
-}
+
