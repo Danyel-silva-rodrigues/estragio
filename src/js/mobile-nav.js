14bc77fd8e68d43c3fa99843e2284587dbd03d04
Danyel-silva-rodrigues <155700370+Danyel-silva-rodrigues@users.noreply.github.com>
2024-09-24 22:59:40 -0300

    quase finalizando a pagina de estagio

---
+++ src/js/mobile-nav.js
@@ -0,0 +1,9 @@
+
+let hamburger = document.querySelector('.hamburger');
+let navBar = document.querySelector('.nav-bar');
+
+
+    hamburger.addEventListener('click', () => {
+        navBar.classList.toggle('active');
+        
+    });
